e1b26c51a8246a2ab88cbaa99cd1725e748c45bc
dev-moshiur <dev.moshiurr@gmail.com>
2023-08-05 00:46:32 +0600

    commited

--- data/profileInfo.js
+++ data/profileInfo.js
@@ -1,7 +1,7 @@
 export const profileInfo = {
-  name: "Monalisa Ashley",
-  title: "Ui/Ux Designer",
-  imageSrcOne: "/assets/img/parsonal-info/parson-img-1.png",
-  imageSrcTwo: "/assets/img/parsonal-info/parson-img-3.png",
-  imageSrcThree: "/assets/img/parsonal-info/parson-img-2.png",
+  name: "Md Moshiur Rahman",
+  title: "MERN stack developer",
+
+  imageSrcTwo: "/assets/img/parsonal-info/dev-moshiur.png",
+
 };
