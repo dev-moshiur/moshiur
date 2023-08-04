c67ca22db40fa6a31be27283331a35d46f04fba9
dev-moshiur <dev.moshiurr@gmail.com>
2023-08-05 01:15:34 +0600

    commited

--- components/homes/about/AboutThree.jsx
+++ components/homes/about/AboutThree.jsx
@@ -1,19 +1,13 @@
-"use client";
 
-import { items, itemsTwo } from "@/data/jobFeatures";
-import React, { useEffect, useState } from "react";
-import { clientLogos } from "@/data/clientLogos";
 
-import { Navigation, Pagination, Autoplay } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { items } from "@/data/jobFeatures";
+
+
 import { bioData } from "@/data/bioData";
 import Image from "next/image";
 
 export default function AboutThree() {
-  const [showSlider, setShowSlider] = useState(false);
-  useEffect(() => {
-    setShowSlider(true);
-  }, []);
+
   return (
     <div className="bostami-page-content-wrap">
       <div className="section-wrapper pl-60 pr-60 pt-60">
