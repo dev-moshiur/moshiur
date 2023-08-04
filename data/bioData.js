44c2c440294113867a6ae97ea1521e905189cf72
dev-moshiur <dev.moshiurr@gmail.com>
2023-08-05 02:51:11 +0600

    commited

--- data/bioData.js
+++ data/bioData.js
@@ -1,8 +1,32 @@
 export const bioData = {
-  descOne: `I'm Creative Director and UI/UX Designer
-    from Sydney, Australia, working in web development and print media. I enjoy
-    turning complex problems into simple, beautiful and intuitive designs.`,
-  descTwo: `My aim is to bring across your message and
-    identity in the most creative way. I created web design for many famous brand
-    companies.`,
+  descOne: (
+    <>
+    Hello! My name is Md Moshiur Rahman Masud and I am a{" "}
+    <strong> MERN stack developer</strong>. I began my journey in 2020
+    during the challenging time of the COVID-19 pandemic. Since then, I
+    have honed my skills in the{" "}
+    <strong>
+      {" "}
+      MERN (MongoDB, Express.js, React.js, and Node.js) stack
+    </strong>
+    , which is a powerful and popular technology stack for building web
+    applications. I have a strong foundation in front-end development
+    with<strong> React.js</strong> and back-end development with{" "}
+    <strong>Node.js and Express.js</strong> , as well as experience
+    working with <strong>MongoDB(Mongoose)</strong> for database
+    management. I am passionate about creating efficient and
+    user-friendly web applications that provide a seamless experience
+    for users. I am excited to continue my journey as a MERN stack
+    developer and contribute to innovative projects in the tech
+    industry.</>
+  ),
+  descTwo: (
+    <>
+    I am passionate about creating efficient and
+    user-friendly web applications that provide a seamless experience
+    for users. I am excited to continue my journey as a MERN stack
+    developer and contribute to innovative projects in the tech
+    industry.
+    </>
+  ),
 };
