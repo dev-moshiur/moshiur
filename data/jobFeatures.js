80f06b8322f8072bc4920c6c82e08f7f70822a29
dev-moshiur <dev.moshiurr@gmail.com>
2023-08-05 03:01:41 +0600

    commited

--- data/jobFeatures.js
+++ data/jobFeatures.js
@@ -4,7 +4,9 @@ export const items = [
     icon: "/assets/img/webdev.svg",
     title: "Web Development",
     description:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod tincidunt volutpat.",
+      ` My expertise lies in building the frontend with React and Next.js, where I combine my skills in CSS, Sass, and Bootstrap to craft beautiful and responsive user interfaces.
+
+      On the backend, I rely on Node.js, Express.js, and MongoDB with Mongoose to develop scalable and efficient server-side solutions. I take pride in my ability to architect and implement robust APIs that power seamless interactions between the frontend and backend.`,
     bg: "bg-prink",
   },
   // {
@@ -20,7 +22,9 @@ export const items = [
     icon: "/assets/img/app-development.svg",
     title: "Mobile App Development",
     description:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod tincidunt volutpat.",
+      `As a mobile app developer with expertise in React Native, I bring a unique set of skills and experiences to the table. In a mobile app development job interview, I would express myself as follows:
+
+      I am a passionate and versatile mobile app developer with a strong focus on building exceptional experiences using React Native. My proficiency in JavaScript and React allows me to create cross-platform applications that run seamlessly on both iOS and Android devices.`,
     bg: "bg-catkrill",
   },
   // {
