c1474c652ea4aaf8468ee458a6119d028eec5eec
dev-moshiur <dev.moshiurr@gmail.com>
2023-08-05 03:02:44 +0600

    commited

--- data/jobFeatures.js
+++ data/jobFeatures.js
@@ -22,7 +22,7 @@ export const items = [
     icon: "/assets/img/app-development.svg",
     title: "Mobile App Development",
     description:
-      `As a mobile app developer with expertise in React Native, I bring a unique set of skills and experiences to the table. In a mobile app development job interview, I would express myself as follows:
+      `
 
       I am a passionate and versatile mobile app developer with a strong focus on building exceptional experiences using React Native. My proficiency in JavaScript and React allows me to create cross-platform applications that run seamlessly on both iOS and Android devices.`,
     bg: "bg-catkrill",
