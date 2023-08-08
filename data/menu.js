7d33d7f7f60437ca7c49ce2fb209bf8883a7599b
dev-moshiur <dev.moshiurr@gmail.com>
2023-08-08 20:41:56 +0600

    commited

--- data/menu.js
+++ data/menu.js
@@ -2,7 +2,7 @@ export const menuItems = [
   { id: 1, href: "/", icon: "fa-light fa-user", text: "About" },
   { id: 2, href: "/resume", icon: "fa-light fa-file-lines", text: "Resume" },
   { id: 3, href: "/portfolio", icon: "fa-light fa-briefcase", text: "Works" },
-  { id: 4, href: "/blog", icon: "fa-brands fa-blogger", text: "Blogs" },
+  // { id: 4, href: "/blog", icon: "fa-brands fa-blogger", text: "Blogs" },
   {
     id: 5,
     href: "/contact",
